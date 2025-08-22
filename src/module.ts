4e0a0bcbc4b02c87eb7bb2fbe912a66a29b9da52
PerakZPercic <120364918+PerakZPercic@users.noreply.github.com>
2025-08-22 12:20:00 +0200

    Push

--- src/module.ts
+++ src/module.ts
@@ -13,7 +13,35 @@ export const plugin = new PanelPlugin<VizualOptions>(VizualPanel).useFieldConfig
     FieldConfigProperty.Unit,
     FieldConfigProperty.Actions,
     FieldConfigProperty.NoValue
-  ]
+  ],
+  standardOptions: {
+    [FieldConfigProperty.DisplayName]: {
+      hideFromDefaults: true
+    },
+    [FieldConfigProperty.Color]: {
+      hideFromDefaults: true
+    },
+    [FieldConfigProperty.Links]: {
+      hideFromDefaults: true
+    },
+    [FieldConfigProperty.Mappings]: {
+      hideFromDefaults: true
+    }
+  },
+  useCustomConfig(builder) {
+    return builder.addSelect({
+      path: "test",
+      name: "Value",
+      settings: {
+        allowCustomValue: false,
+        options: [
+          {label: "Test", value: ""},
+          {label: "test2", value: 3}
+        ]
+      },
+      hideFromDefaults: true
+    })
+  },
 })
 .setPanelOptions(bldr => {
   return bldr.addNumberInput({
